a907dc2896d27ad160b30ce8d4e7d962ed5b03e5
LimitToInfinity <21stLimitless@gmail.com>
2021-03-25 14:05:27 -0600

    Brings truck stops from backend.

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const truckstops = [
-  { title: "Forest", location: "Colorado Springs" },
-  { title: "Pavement", location: "Denver" },
-  { title: "Mountain", location: "Breckenridge" },
-]
+const baseURL = 'http://localhost:3000'
+const truckStopsURL = `${baseURL}/truck_stops`
 
-const truckstopsContainer = document.querySelector('#truckstops-container')
-console.log('truckstops container', truckstopsContainer)
-truckstops.forEach(truckstop => {
-  const truckstopCard = document.createElement('div')
-  truckstopCard.classList.add('truckstop-card')
-  
-  const truckstopTitle = document.createElement('h2')
-  truckstopTitle.classList.add('truckstop-title')
-  truckstopTitle.textContent = truckstop.title
-  
-  const truckstopLocation = document.createElement('p')
-  truckstopLocation.classList.add('truckstop-location')
-  truckstopLocation.textContent = truckstop.location
-
-  truckstopCard.append(truckstopTitle, truckstopLocation)
-  truckstopsContainer.append(truckstopCard)
-})
+fetch(truckStopsURL)
+  .then(response => response.json())
+  .then(truckStops => {
+    const truckStopsContainer = document.querySelector('#truckstops-container')
+    truckStops.forEach(truckStop => {
+      const truckStopCard = document.createElement('div')
+      truckStopCard.classList.add('truckstop-card')
+      
+      const truckStopTitle = document.createElement('h2')
+      truckStopTitle.classList.add('truckstop-title')
+      truckStopTitle.textContent = truckStop.title
+      
+      const truckStopLocation = document.createElement('p')
+      truckStopLocation.classList.add('truckstop-location')
+      truckStopLocation.textContent = truckStop.location
+    
+      truckStopCard.append(truckStopTitle, truckStopLocation)
+      truckStopsContainer.append(truckStopCard)
+    })
+  })
